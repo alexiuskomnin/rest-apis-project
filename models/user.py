8e3244cd908bcd5d5c12f12639b1c39ef7027f6e
Alex(Oleksii Yuvchenko) <alex@lap2281.hlit.local>
2023-07-19 14:45:20 +0300

    Added email functionality

--- models/user.py
+++ models/user.py
@@ -6,3 +6,4 @@ class UserModel(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(80), unique=True, nullable=False)
   password = db.Column(db.String(256), unique=True, nullable=False)
+  email = db.Column(db.String, unique=True, nullable=False)
